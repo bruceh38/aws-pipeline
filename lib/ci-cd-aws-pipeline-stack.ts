f2e64a478b9d51714588ff6e7fe888147ae4f47b
bruceh38 <yanxiao.huang@tufts.edu>
2026-04-14 22:31:40 -0400

    add full stages

--- lib/ci-cd-aws-pipeline-stack.ts
+++ lib/ci-cd-aws-pipeline-stack.ts
@@ -2,12 +2,13 @@ import * as cdk from 'aws-cdk-lib'
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';  
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './stage';
 
 export class CiCdAwsPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline', 
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('bruceh38/aws-pipeline', 'main'), 
@@ -17,5 +18,15 @@ export class CiCdAwsPipelineStack extends cdk.Stack {
         
       }),
     });
+
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "410543808434", region: "us-east-1" }
+    }));
+
+    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+
+    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+      env: { account: "410543808434", region: "us-east-1" }
+    }));
   }
 }
